09c216c3841fe3c67979cd7a313c9fc74df42e27
Brk <brkgamerr@gmail.com>
2025-02-17 13:07:51 +0100

    Added Gradebook-App project.

--- Learning-Projects/Gradebook-App/script.js
+++ Learning-Projects/Gradebook-App/script.js
@@ -1,8 +1,8 @@
 function getAverage(scores) {
     let sum_of_scores = 0;
 
-    for (let i = 0; i < scores.length; i++) {
-        sum_of_scores += scores[i];
+    for (const score of scores) {
+        sum_of_scores += score;
     }
 
     return sum_of_scores / scores.length;
@@ -13,16 +13,16 @@ function getGrade(score) {
     if (score >= 100) {
         return "A++";
     }
-    else if (score >= 90 && score < 100) {
+    else if (score >= 90) {
         return "A";
     }
-    else if (score >= 80 && score <90) {
+    else if (score >= 80) {
         return "B";
     }
-    else if (score >=70 && score < 80) {
+    else if (score >=70) {
         return "C";
     }
-    else if (score >= 60 && score <70) {
+    else if (score >= 60) {
         return "D";
     }
     else {
@@ -39,11 +39,11 @@ function hasPassingGrade(score) {
 function studentMsg(totalScores, studentScore) {
     if (hasPassingGrade(studentScore) === true) {
         return "Class average: " + getAverage(totalScores) +
-            ". Your grade: " + getGrade(studentScore) + ". You passed the course."
+            ". Your grade: " + getGrade(studentScore) + ". You passed the course.";
     }
     else {
         return "Class average: " + getAverage(totalScores) +
-            ". Your grade: " + getGrade(studentScore) + ". You failed the course."
+            ". Your grade: " + getGrade(studentScore) + ". You failed the course.";
     }
 }
 
